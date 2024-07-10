267a909ee31229b7dcceaae301b95270d0ec086d
chadgrover <93070367+chadgrover@users.noreply.github.com>
2024-07-10 16:09:58 +0900

    style: Replace Storybook preview white background with gradient

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,15 @@ import "@/app/globals.css";
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      default: "greatFrontEndBackground",
+      values: [
+        {
+          name: "greatFrontEndBackground",
+          value: "linear-gradient(to bottom, #f9fafb, #d2d6db)",
+        },
+      ],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
